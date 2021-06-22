31925b8731cf2eb461fb2e76503f76b4aaf0491e
shanxman1986@gmail.com <ubuntu@ip-172-31-22-181.ec2.internal>
2021-06-22 14:13:23 +0000

    more file added in client

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,13 +1,13 @@
-import React, ( Component ) from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-class Input extends component {
+class Input extends Component {
 	state = {
 		action: ""
 	}
 
 	addTodo = () => {
-		const task = (action: this.state.action)
+		const task = {action: this.state.action}
 
 		if(task.action && task.action.length > 0){
 			axios.post('/api/todos', task)
@@ -17,21 +17,28 @@ class Input extends component {
 					this.setState({action: ""})
 				}
 			})
-			.catch(err => console.log(err)}
+			.catch(err => console.log(err))
 	}else {
 		console.log('input field required')
 	}
 }
 
-handleChange = (e) {
+handleChange = (e) =>  {
+	this.setState({
+		action: e.target.value
+	})
+}
+
+render(){
 	let { action } = this.state;
-	return {
+	return (
 		<div>
 		<input type="text" onChange={this.handleChange} value={action} />
 		<button onClick={this.addTodo}>add todo</button>
 		</div>
 			)
-	}
+
+}
 }
 
 export default Input
